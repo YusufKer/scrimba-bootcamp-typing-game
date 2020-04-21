50436f382ba049c32ff0d3bc8b5a117f1236dbbc
Yusuf Keraan <yus.ker1994@gmail.com>
2020-04-21 13:00:36 +0200

    Updates

---
+++ precache-manifest.010bbfe0c802a187cf74ea8d95b3ede1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e48b8df7b9393e02e12a2eb2f9001c",
+    "url": "/scrimba-bootcamp-typing-game/index.html"
+  },
+  {
+    "revision": "e55bb4713ae7eb3c7c1f",
+    "url": "/scrimba-bootcamp-typing-game/static/css/main.ae866f4d.chunk.css"
+  },
+  {
+    "revision": "fe82141a3ac382d433a7",
+    "url": "/scrimba-bootcamp-typing-game/static/js/2.fd877b43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scrimba-bootcamp-typing-game/static/js/2.fd877b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55bb4713ae7eb3c7c1f",
+    "url": "/scrimba-bootcamp-typing-game/static/js/main.d4fc72ee.chunk.js"
+  },
+  {
+    "revision": "8bcb8617eef80701e4d8",
+    "url": "/scrimba-bootcamp-typing-game/static/js/runtime-main.cc09d35d.js"
+  }
+]);
